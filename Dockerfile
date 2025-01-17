70f35c8f3461c99917f97d70fd30ab02a5087c79
April-May <18632637+AprilMay0@users.noreply.github.com>
2025-01-17 00:54:02 +0000

    try apt-get

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,9 @@ FROM ubuntu:latest
 RUN apt-get update && apt-get install -y \
   python3.10 \
   python3-pip \
-  python3-venv \
+  python3-yaml \
   git
 
-# Create a virtual environment
-RUN python3 -m venv /venv
-
-# Install PyYAML within the virtual environment
-RUN /venv/bin/pip install PyYAML
 
 COPY feed.py /usr/bin/feed.py
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-# Run your Python script using the Python interpreter from the virtual environment
-/venv/bin/python /usr/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
 
